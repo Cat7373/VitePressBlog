c771f615102e266d93bdb89e0e251674d2d46abe
Cat73 <root@cat73.org>
2024-02-27 17:13:59 +0800

    :sparkles: 切换文章时，正确的重新加载评论插件

--- src/.vitepress/theme/layout/Index.vue
+++ src/.vitepress/theme/layout/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <template #doc-after>
-      <div id="gitalk-container"></div>
+      <div v-if="initGitalkStep" id="gitalk-container"></div>
     </template>
   </Layout>
 </template>
@@ -9,13 +9,34 @@
 <script lang="ts" setup>
 import { useRoute } from 'vitepress'
 import Theme from 'vitepress/theme'
-import { watch, onMounted } from 'vue'
+import { ref, watch, onMounted, nextTick } from 'vue'
 import Gitalk from 'gitalk'
 
 const { Layout } = Theme
 const route = useRoute()
+// 当前加载状态
+// 0 DOM 中无元素，此时调用应将元素插入到 DOM 中，等下个 step 再加载
+// 1 DOM 中有元素，此时调用应用已有 DOM 元素初始化评论插件，加载后步骤完成，不需要再做什么了
+// 2 插件已经加载，此时调用应是切换页面了，应删除页面中的 DOM 元素，等下个 step 再插入
+const initGitalkStep = ref(0)
 
 const initGitalk = () => {
+  // 切换页面时，刷新评论组件
+  switch (initGitalkStep.value) {
+    case 0: // DOM 中无元素，此时调用应将元素插入到 DOM 中，等下个 step 再加载
+      initGitalkStep.value = 1
+      nextTick(initGitalk)
+      return
+    case 1: // DOM 中有元素，此时调用应用已有 DOM 元素初始化评论插件，加载后步骤完成，不需要再做什么了
+      initGitalkStep.value = 2
+      break
+    case 2: // 插件已经加载，此时调用应是切换页面了，应删除页面中的 DOM 元素，等下个 step 再插入
+      initGitalkStep.value = 0
+      nextTick(initGitalk)
+      return
+  }
+
+  // 创建评论组件
   const gitTalk = new Gitalk({
     // GitHub 账号
     owner: 'Cat7373',
@@ -33,11 +54,14 @@ const initGitalk = () => {
     createIssueManually: true,
     // 创建 Issue 时，用于唯一标识这篇文章的标记
     id: location.pathname,
+    // 撰写评论时，给一个全屏遮罩，聚焦到评论框
+    distractionFreeMode: true,
   })
-  // 初始化评论插件
+  // 渲染到 DOM 元素中
   gitTalk.render('gitalk-container')
 }
 
+// 初始化和页面切换时加载评论插件
 onMounted(initGitalk)
 watch(
   () => route.path,
